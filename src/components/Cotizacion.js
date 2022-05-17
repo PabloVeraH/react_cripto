d4e88aad33d83e6aba8c171d47f1f7d3b86184fe
Pablo Vera <pablo.vera82@gmail.com>
2022-05-16 21:00:22 -0400

    Add style to API result

--- src/components/Cotizacion.js
+++ src/components/Cotizacion.js
@@ -1,4 +1,23 @@
 import React from 'react';
+import styled from '@emotion/styled';
+
+const ResultadoDiv = styled.div`
+    color: #FFF;
+    font-family: Arial, Helvetica, sans-serif;
+`;
+
+const Info = styled.p`
+    font-size: 18px;
+    span {
+        font-weight:bold;
+    }
+`;
+const Precio = styled.p`
+    font-size: 30px;
+    span {
+        font-weight:bold;
+    }
+`
 
 const Cotizacion = ({resultado}) => {
     if(Object.keys(resultado).length === 0) return null;
@@ -6,13 +25,13 @@ const Cotizacion = ({resultado}) => {
     console.log(resultado)
 
     return ( 
-        <div>
-            <p>El precio es: <span>{resultado.PRICE}</span> </p>
-            <p>Precio más alto del día: <span>{resultado.HIGHDAY}</span> </p>
-            <p>Precio más bajo del día: <span>{resultado.LOWDAY}</span> </p>
-            <p>Variación últimas 24 horas: <span>{resultado.CHANGEPCT24HOUR}</span> </p>
-            <p>Última Actualización: <span>{resultado.LASTUPDATE}</span> </p>
-        </div>
+        <ResultadoDiv>
+            <Precio>The price is: <span>{resultado.PRICE}</span> </Precio>
+            <Info>Highest price of the day: <span>{resultado.HIGHDAY}</span> </Info>
+            <Info>Lowest price of the day: <span>{resultado.LOWDAY}</span> </Info>
+            <Info>Variation last 24 hours: <span>{resultado.CHANGEPCT24HOUR}</span> </Info>
+            <Info>Last update: <span>{resultado.LASTUPDATE}</span> </Info>
+        </ResultadoDiv>
      );
 }
 
